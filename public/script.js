095f755e3ada146713efd720217109a95fa70362
Miyazono45 <rifqiromadhoni44@gmail.com>
2023-09-21 19:59:48 +0700

    Small MediaQuery Change

--- public/script.js
+++ public/script.js
@@ -1,33 +1,11 @@
-const fbA = document.querySelector('.facebookA')
-const twitA = document.querySelector('.twitterA')
-const igA = document.querySelector('.instagramA')
+const arrowSvg = document.querySelector("svg");
+window.addEventListener("resize", function () {
+  const windowWidth = window.innerWidth;
 
-const copacopa = document.querySelector('svg')
-window.addEventListener('resize', function () {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    // Set the SVG fill color based on the window width and height.
-    if (windowWidth >= 768) {
-        copacopa.setAttribute('fill', 'green');
-    } else {
-        copacopa.setAttribute('fill', 'red');
-    }
+  // Set the SVG fill color based on the window width and height.
+  if (windowWidth >= 768) {
+    arrowSvg.setAttribute("fill", "#685bf6");
+  } else {
+    arrowSvg.setAttribute("fill", "#f0eff1");
+  }
 });
-
-
-// $(window).resize(function () {
-//     let mqTab = window.matchMedia("(min-width: 768px)")
-//     if (mqTab.matches) {
-//         igA.setAttribute("fill", "#685bf6")
-//         twitA.setAttribute("fill", "#685bf6")
-//         fbA.setAttribute("fill", "#685bf6")
-//     } else {
-//         igA.setAttribute("fill", "#685bf6")
-//         twitA.setAttribute("fill", "#f0eff1")
-//         fbA.setAttribute("fill", "#f0eff1")
-//     }
-// });
-
-
-
